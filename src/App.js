d0a671f4530bd7a45361c71a7e0a128e519c9980
KevinSadler <KevinSadler@github.comgit config --global user.email KevinSadler@github.comgit config --global user.email KevinSadler@github.com>
2022-03-13 16:53:28 -0500

    initial setup of app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [myHeader, setHeader] = useState("Start");
+  const handleInput = event => {
+    setHeader(event.target.value)
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
+      <h1>{myHeader}</h1>
+      <input id='headerInput' type={'text'} onChange={handleInput}></input>
+      {/* <button type='button' onClick={() => setHeader("test")}>Set Header</button> */}
       </header>
     </div>
   );
