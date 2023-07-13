2f307e04c174f4b41ef89a3b08dd109de8199c7e
Kevin Sadler <ksadler@healthcarebluebook.com>
2023-07-13 15:51:50 -0500

    almost mvp

--- src/game.tsx
+++ src/game.tsx
@@ -1,94 +1,152 @@
-import React, { useEffect, useState } from 'react';
+import React, { ButtonHTMLAttributes, useCallback, useEffect, useRef, useState } from 'react';
+import Modal from 'react-modal';
 import './App.css';
 
 function Game() {
     const [attempts, updateAttempts] = useState<{ [key: string]: string }>({});
     const [currentGuess, setCurrentGuess] = useState<string>("");
     const [rowIndex, setRowIndex] = useState(1);
-
-    // useEffect(() => {
-    //     if (attempts.length === 6) {
-    //         resetGame();
-    //     }
-    // })
+    const [gameWon, setGameWon] = useState<boolean>(false);
+    const [modalOpen, setModalOpen] = useState<boolean>(true);
+    const inputRef = useRef<HTMLInputElement | null>(null);
+    const newGameButtonRef = useRef<HTMLButtonElement | null>(null);
+    const answer = 'BUILD';
+    const gif = require('./Fireworks.gif')
 
     const handleInput = event => {
-        setCurrentGuess(event.target.value);
-        // var newState = {} as any;
-        for (var i = 0; i < event.target.value.length; i++) {
-            var myKey = `r${rowIndex}c${i + 1}`;
-            var value = event.target.value.charAt(i);
-            // newState[myKey] = value;
-            updateAttempts({...attempts, [myKey]: value});
-        }
-        // console.log("newState: ", newState);
+        let value: string = event.target.value;
+        setCurrentGuess(value.toUpperCase());
     };
 
-    // const handleSave = () => {
-    //     if (currentGuess.length === 5) {
-    //         const newList = [
-    //             ...attempts, currentGuess
-    //         ]
-    //         updateAttempts(newList);
-    //         setCurrentGuess("");
-    //     }
-    // }
+    const handleSave = () => {
+        if (currentGuess.length === 5) {
+            var newAttempt: { [key: string]: string } = {};
+            for (var i = 0; i < currentGuess.length; i++) {
+                var key = `r${rowIndex}c${i + 1}`;
+                var value = currentGuess.charAt(i);
+                newAttempt = ({...newAttempt, [key]: value});
+                if (answer.charAt(i) === value) {
+                    setClass(key, "correctPosition")
+                } else if (answer.includes(value)) {
+                    setClass(key, "incorrectPosition")
+                } else {
+                    setClass(key, "incorrect")
+                }
+                if (currentGuess === answer) {
+                    setGameWon(true);
+                }
+            };
+            let updatedAttempts = Object.assign(newAttempt, attempts);
+            updateAttempts(updatedAttempts);
+            setCurrentGuess("");
+            setRowIndex(rowIndex + 1);
+        }
+    }
 
-    // const resetGame = () => {
-    //     setCurrentGuess("");
-    //     updateAttempts([]);
-    // }
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            handleSave();
+        }
+    }
+
+    const handleNewGame = () => {
+        setModalOpen(false);
+        console.log(modalOpen);
+    }
+
+    useEffect(() => {
+        document.addEventListener("click", setFocus);
+    });
+    
+    const setFocus = (e) => {
+        if (!newGameButtonRef.current?.contains(e.target)) {
+            inputRef.current?.focus();
+            console.log("clicked outside of button");
+        } else {
+            console.log("Clicked new game button");
+        }
+    };
+
+    const setClass = (id: string, className: string) => {
+        document.getElementById(id)?.classList.add(className);
+    }
+
+    const logValues = () => {
+        console.log("attempts: ", attempts);
+        console.log("current guess: ", currentGuess);
+        console.log("row index: ", rowIndex);
+    }
 
     return (
         <div className="App-body">
+            {gameWon && 
+                <img src={gif}/> 
+            }
+            <Modal
+                id="new-game-button"
+                className="new-game-modal"
+                overlayClassName="new-game-modal"
+                isOpen={modalOpen}>
+                <div >
+                    <button ref={newGameButtonRef} onClick={() => handleNewGame()} className='new-game-button'>New Game?</button>
+                </div>
+            </Modal>
             <div className='row'>
-                <div className='letterBox' id='r1c1'>{attempts["r1c1"]}</div>
-                <div className='letterBox' id='r1c2'>{attempts["r1c2"]}</div>
-                <div className='letterBox' id='r1c3'>{attempts["r1c3"]}</div>
-                <div className='letterBox' id='r1c4'>{attempts["r1c4"]}</div>
-                <div className='letterBox' id='r1c5'>{attempts["r1c5"]}</div>
+                <div className='letterBox' id='r1c1'>{rowIndex === 1 ? currentGuess[0] : attempts["r1c1"]}</div>
+                <div className='letterBox' id='r1c2'>{rowIndex === 1 ? currentGuess[1] : attempts["r1c2"]}</div>
+                <div className='letterBox' id='r1c3'>{rowIndex === 1 ? currentGuess[2] : attempts["r1c3"]}</div>
+                <div className='letterBox' id='r1c4'>{rowIndex === 1 ? currentGuess[3] : attempts["r1c4"]}</div>
+                <div className='letterBox' id='r1c5'>{rowIndex === 1 ? currentGuess[4] : attempts["r1c5"]}</div>
             </div>
             <div className='row'>
-                <div className='letterBox' id='r2c1'>{attempts["r2c1"]}</div>
-                <div className='letterBox' id='r2c2'>{attempts["r2c2"]}</div>
-                <div className='letterBox' id='r2c3'>{attempts["r2c3"]}</div>
-                <div className='letterBox' id='r2c4'>{attempts["r2c4"]}</div>
-                <div className='letterBox' id='r2c5'>{attempts["r2c5"]}</div>
+                <div className='letterBox' id='r2c1'>{rowIndex === 2 ? currentGuess[0] : attempts["r2c1"]}</div>
+                <div className='letterBox' id='r2c2'>{rowIndex === 2 ? currentGuess[1] : attempts["r2c2"]}</div>
+                <div className='letterBox' id='r2c3'>{rowIndex === 2 ? currentGuess[2] : attempts["r2c3"]}</div>
+                <div className='letterBox' id='r2c4'>{rowIndex === 2 ? currentGuess[3] : attempts["r2c4"]}</div>
+                <div className='letterBox' id='r2c5'>{rowIndex === 2 ? currentGuess[4] : attempts["r2c5"]}</div>
             </div>
             <div className='row'>
-                <div className='letterBox' id='r3c1'>{attempts["r3c1"]}</div>
-                <div className='letterBox' id='r3c2'>{attempts["r3c2"]}</div>
-                <div className='letterBox' id='r3c3'>{attempts["r3c3"]}</div>
-                <div className='letterBox' id='r3c4'>{attempts["r3c4"]}</div>
-                <div className='letterBox' id='r3c5'>{attempts["r3c5"]}</div>
+                <div className='letterBox' id='r3c1'>{rowIndex === 3 ? currentGuess[0] : attempts["r3c1"]}</div>
+                <div className='letterBox' id='r3c2'>{rowIndex === 3 ? currentGuess[1] : attempts["r3c2"]}</div>
+                <div className='letterBox' id='r3c3'>{rowIndex === 3 ? currentGuess[2] : attempts["r3c3"]}</div>
+                <div className='letterBox' id='r3c4'>{rowIndex === 3 ? currentGuess[3] : attempts["r3c4"]}</div>
+                <div className='letterBox' id='r3c5'>{rowIndex === 3 ? currentGuess[4] : attempts["r3c5"]}</div>
             </div>
             <div className='row'>
-                <div className='letterBox' id='r4c1'>{attempts["r4c1"]}</div>
-                <div className='letterBox' id='r4c2'>{attempts["r4c2"]}</div>
-                <div className='letterBox' id='r4c3'>{attempts["r4c3"]}</div>
-                <div className='letterBox' id='r4c4'>{attempts["r4c4"]}</div>
-                <div className='letterBox' id='r4c5'>{attempts["r4c5"]}</div>
+                <div className='letterBox' id='r4c1'>{rowIndex === 4 ? currentGuess[0] : attempts["r4c1"]}</div>
+                <div className='letterBox' id='r4c2'>{rowIndex === 4 ? currentGuess[1] : attempts["r4c2"]}</div>
+                <div className='letterBox' id='r4c3'>{rowIndex === 4 ? currentGuess[2] : attempts["r4c3"]}</div>
+                <div className='letterBox' id='r4c4'>{rowIndex === 4 ? currentGuess[3] : attempts["r4c4"]}</div>
+                <div className='letterBox' id='r4c5'>{rowIndex === 4 ? currentGuess[4] : attempts["r4c5"]}</div>
             </div>
             <div className='row'>
-                <div className='letterBox' id='r5c1'>{attempts["r5c1"]}</div>
-                <div className='letterBox' id='r5c2'>{attempts["r5c2"]}</div>
-                <div className='letterBox' id='r5c3'>{attempts["r5c3"]}</div>
-                <div className='letterBox' id='r5c4'>{attempts["r5c4"]}</div>
-                <div className='letterBox' id='r5c5'>{attempts["r5c5"]}</div>
+                <div className='letterBox' id='r5c1'>{rowIndex === 5 ? currentGuess[0] : attempts["r5c1"]}</div>
+                <div className='letterBox' id='r5c2'>{rowIndex === 5 ? currentGuess[1] : attempts["r5c2"]}</div>
+                <div className='letterBox' id='r5c3'>{rowIndex === 5 ? currentGuess[2] : attempts["r5c3"]}</div>
+                <div className='letterBox' id='r5c4'>{rowIndex === 5 ? currentGuess[3] : attempts["r5c4"]}</div>
+                <div className='letterBox' id='r5c5'>{rowIndex === 5 ? currentGuess[4] : attempts["r5c5"]}</div>
             </div>
             <div className='row'>
-                <div className='letterBox' id='r6c1'>{attempts["r6c1"]}</div>
-                <div className='letterBox' id='r6c2'>{attempts["r6c2"]}</div>
-                <div className='letterBox' id='r6c3'>{attempts["r6c3"]}</div>
-                <div className='letterBox' id='r6c4'>{attempts["r6c4"]}</div>
-                <div className='letterBox' id='r6c5'>{attempts["r6c5"]}</div>
+                <div className='letterBox' id='r6c1'>{rowIndex === 6 ? currentGuess[0] : attempts["r6c1"]}</div>
+                <div className='letterBox' id='r6c2'>{rowIndex === 6 ? currentGuess[1] : attempts["r6c2"]}</div>
+                <div className='letterBox' id='r6c3'>{rowIndex === 6 ? currentGuess[2] : attempts["r6c3"]}</div>
+                <div className='letterBox' id='r6c4'>{rowIndex === 6 ? currentGuess[3] : attempts["r6c4"]}</div>
+                <div className='letterBox' id='r6c5'>{rowIndex === 6 ? currentGuess[4] : attempts["r6c5"]}</div>
             </div>
-            <h1>Current Guess: {currentGuess}</h1>
-            <input type={'text'} value={currentGuess} onChange={handleInput} maxLength={5}></input>
-            {/* <button type='button' onClick={() => handleSave()}>Save</button> */}
-            <button type='button' onClick={() => console.log(attempts)}>Log</button>
-            {/* <button type='button' onClick={() => console.log(currentGuess)}>current guess</button> */}
+            <input 
+                ref={inputRef} 
+                id='inputField' 
+                type={'text'} 
+                value={currentGuess} 
+                onChange={handleInput} 
+                maxLength={5} 
+                autoFocus 
+                onKeyDown={(e) => handleKeyPress(e)}>
+            </input>
+            {/* <button type='button' onClick={() => logValues()}>Log</button> */}
+            <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
         </div>
+
     );
 };
 
